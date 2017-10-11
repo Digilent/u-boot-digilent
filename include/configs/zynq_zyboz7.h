8875fa217e601fc57272526975ed0b7b48b85629
Sam Bobrowicz <sbobrowicz@digilentinc.com>
2017-10-11 12:25:53 -0700

    Add Digilent Zybo Z7 board support

---
+++ include/configs/zynq_zyboz7.h
@@ -0,0 +1,24 @@
+/*
+ * (C) Copyright 2016 Digilent Inc.
+ *
+ * Configuration for Zynq Development Board - ARTY Z7
+ * See zynq-common.h for Zynq common configs
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __CONFIG_ZYNQ_ARTYZ7_H
+#define __CONFIG_ZYNQ_ARTYZ7_H
+
+#define CONFIG_SYS_NO_FLASH
+#define CONFIG_ZYNQ_QSPI
+
+#define CONFIG_ZYNQ_USB
+#define CONFIG_ZYNQ_I2C0
+
+/* GEM MAC address offset */
+#define CONFIG_ZYNQ_GEM_SPI_MAC_OFFSET	0x20
+
+#include <configs/zynq-common.h>
+
+#endif /* __CONFIG_ZYNQ_ARTYZ7_H */
