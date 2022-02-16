06537ac57f28a6320d133e3837787e4ab39f5ddf
Elod Gyorgy <elod.gyorgy@digilent.ro>
2022-02-16 17:58:49 +0200

    realtek:phy: Add LED configuration support through devicetree
    The Realtek RTL8211F driver sets non-default LED behavior:
    	LED2=1000Mbps Link/Activity
    	LED1=Link
    	LED0=Activity.
    This can be overridden from the device-tree.

---
+++ include/dt-bindings/phy/realtek.h
@@ -0,0 +1,13 @@
+/*
+ Constants for Realtek PHY LED configuration modes
+*/
+#ifndef _DT_BINDINGS_PHY_REALTEK
+#define _DT_BINDINGS_PHY_REALTEK
+
+#define REALTEK_LED_LINK10              BIT(0)
+#define REALTEK_LED_LINK100             BIT(1)
+#define REALTEK_LED_LINK1000            BIT(3)
+#define REALTEK_LED_ACT                 BIT(4)
+#define REALTEK_LED_DEFAULT             BIT(7)
+
+#endif /* _DT_BINDINGS_PHY_REALTEK */
